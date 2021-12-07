6651688e8e9d1c95415f344dab7c1823ff8a70e1
Tomer Ashkenazy <rfmsec@gmail.com>
2021-12-07 12:05:00 +0200

    Updated network_scanner.py

--- learn-ethical-python/network-scanner/network_scanner.py
+++ learn-ethical-python/network-scanner/network_scanner.py
@@ -7,10 +7,21 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")                                  # Create Ethernet layer
     arp_request_broadcast = broadcast/arp_request                                     # Stack the layers
     answered_list = scapy.srp(arp_request_broadcast, timeout=2,verbose=False)[0]      # Capture the results (Scapy returns two lists, we're interested only in the first (answered))
-    print("IP Address\t\t\tMAC Adderss")
-    print("-------------------------------------------------")
+ 
+    scan_results = []
 
     for element in answered_list:                                                     # Parsing the list (Return only meaningful values)
-        print(element[1].psrc + "\t\t\t" + element[1].hwsrc)
+        scan_results.append({"ip": element[1].psrc, "mac":element[1].hwsrc})
 
-scan(input("Please enter Host / Network address: "))
+    return scan_results
+
+def print_results(scan_results):
+    print("IP Address\t\t\tMAC Adderss")
+    print("-------------------------------------------------")
+    for client in scan_results:
+        print(client['ip'] + "\t\t\t" + client['mac'])
+
+
+# scan(input("Please enter Host / Network address: "))
+scan_results = scan("192.168.1.1/24")
+print_results(scan_results)
