e96f1f747d13fda6a812d53f42061f386063e912
Tomer Ashkenazy <rfmsec@gmail.com>
2021-12-21 14:08:01 +0200

    Removed extra space

--- learn-ethical-python/network_scanner/network_scanner.py
+++ learn-ethical-python/network_scanner/network_scanner.py
@@ -17,7 +17,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)                                                  # Create an ARP packet
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")                                  # Create Ethernet layer
     arp_request_broadcast = broadcast/arp_request                                     # Stack the layers
-    answered_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]      # Capture the results (Scapy returns two lists, we're interested only in the first (answered))
+    answered_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]     # Capture the results (Scapy returns two lists, we're interested only in the first (answered))
 
     scan_results = []
 
