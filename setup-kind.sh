a1c93321efe348ee4e4910ba2924ae5e23310c5d
rfmsec <43323904+rfmsec@users.noreply.github.com>
2022-03-30 15:46:02 +0300

    Update setup-kind.sh

--- setup-kind.sh
+++ setup-kind.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 cd /home/ec2-user/
-curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.12.0/kind-linux-amd64
-wget https://github.com/rfmsec/Learning/raw/master/flask-kubeapi.zip
+
+# Download required files
 wget https://raw.githubusercontent.com/rfmsec/Learning/master/kind-config.yaml
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-echo 'export KUBECONFIG="/home/ec2-user/.kube/config"' >> /etc/bashrc
-chmod +x ./kind && mv ./kind /usr/local/bin/
+wget https://github.com/rfmsec/Learning/raw/master/flask-kubeapi.zip
+
+# Install docker
 yum install -y docker
 service docker start
 chkconfig docker on
+
+# Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.12.0/kind-linux-amd64
+chmod +x ./kind && mv ./kind /usr/local/bin/
 /usr/local/bin/kind create cluster --config kind-config.yaml
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install helm and deploy chart
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 unzip flask-kubeapi.zip
 helm install flask-kubeapi ./flask-kubeapi
+
+# Add kubeconfig to bashrc
+echo 'export KUBECONFIG="/home/ec2-user/.kube/config"' >> /etc/bashrc
