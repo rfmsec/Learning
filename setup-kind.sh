f7edd74b0a742eaf150a77c828846e0146cb8aee
rfmsec <43323904+rfmsec@users.noreply.github.com>
2022-03-30 16:40:36 +0300

    Update setup-kind.sh

--- setup-kind.sh
+++ setup-kind.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/
 
-# Download required files
+# Download required files# Add kubeconfig to bashrc
+echo 'export KUBECONFIG="/home/ec2-user/.kube/config"' >> /etc/bashrc
+
 wget https://raw.githubusercontent.com/rfmsec/Learning/master/kind-config.yaml
 wget https://github.com/rfmsec/Learning/raw/master/flask-kubeapi.zip
 
@@ -19,10 +21,10 @@ chmod +x ./kind && mv ./kind /usr/local/bin/
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# Install helm and deploy chart
+# Install helm and unzip chart files
 curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 unzip flask-kubeapi.zip
-helm install flask-kubeapi ./flask-kubeapi
 
-# Add kubeconfig to bashrc
-echo 'export KUBECONFIG="/home/ec2-user/.kube/config"' >> /etc/bashrc
+# Sleep 2 minutes and install helm chart
+sleep 120
+helm install flask-kubeapi ./flask-kubeapi
