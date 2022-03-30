0ccd4d6ae877bca12ee5002fe578ef4a4911f771
rfmsec <43323904+rfmsec@users.noreply.github.com>
2022-03-30 13:35:52 +0300

    Update setup-kind.sh

--- setup-kind.sh
+++ setup-kind.sh
@@ -5,7 +5,7 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.12.0/kind-linux-amd64
 wget https://github.com/rfmsec/Learning/raw/master/flask-kubeapi.zip
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-echo "export KUBECONFIG = '/home/ec2-user/.kube/config'" >> /etc/bashrc
+echo 'export KUBECONFIG="/home/ec2-user/.kube/config"' >> /etc/bashrc
 chmod +x ./kind && mv ./kind /usr/local/bin/
 yum install -y docker
 service docker start
